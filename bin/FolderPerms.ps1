bf0780181b92591c64d684c86efe5314eeb77131
keithdavis <keithdavis@solidtechservice.com>
2013-05-20 11:28:48 -0500

    Modified For Windows Authentication

--- bin/FolderPerms.ps1
+++ bin/FolderPerms.ps1
@@ -1,34 +1,20 @@
 #
 # This snippet gets a dir from ther user, sets owner of thet folder and subfolders to the
-# Administrators group and grants the user IUSR Full access recursively.
+# Administrators group and grants the user IUSR Full access and Users read recursively and enables inheritance.
 #
 #
-
-#Stop IIS
-IISReset /STOP
 
 echo ""
 echo "=======Set Folder Perms======="
 echo ""
 echo ""
-echo "Set Administrators as Owner"
+echo "Set Administrators as Owner, IUSR R/W and Users R"
 echo ""
 
 
 foreach ($i in $args)
 {
     icacls "$i" /setowner administrators /t
+    icacls "$i" /grant:r IUSR:"(OI)(CI)"F /t /inheritance:e
+    icacls "$i" /grant:r Users:"(OI)(CI)"F /t /inheritance:e
 }
-
-echo ""
-echo "Grant IUSR R\W"
-echo ""
-
-
-foreach ($i in $args)
-{
-    icacls "$i" /grant IUSR:"(OI)(CI)"F /t
-}
-
-#Start IIS
-IISReset /START
